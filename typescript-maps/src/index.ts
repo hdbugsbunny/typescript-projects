bb452380e4cb39ac94f9ae37a1a9af09e72f4733
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-21 17:25:19 +0530

    Defining a Company

--- typescript-maps/src/index.ts
+++ typescript-maps/src/index.ts
@@ -1,4 +1,8 @@
+import { Company } from "./Company";
 import { User } from "./User";
 
 const user = new User();
 console.log("🚀 ~ user:", user);
+
+const company = new Company();
+console.log("🚀 ~ company:", company);
