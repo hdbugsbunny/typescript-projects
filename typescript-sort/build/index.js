a4b2745ff927cd5ec4386b1292b4862d08c3b123
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-22 18:34:41 +0530

    Interface Definition & Sorting Arbitrary Collections

--- typescript-sort/build/index.js
+++ typescript-sort/build/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CharactersCollection_1 = require("./CharactersCollection");
 const NumbersCollection_1 = require("./NumbersCollection");
 const Sorter_1 = require("./Sorter");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data); // Output: [-5, 0, 3, 10]
+const charactersCollection = new CharactersCollection_1.CharactersCollection("Hello!");
+const sorterCharacters = new Sorter_1.Sorter(charactersCollection);
+sorterCharacters.sort();
+console.log(charactersCollection.data); // Output: "!eHllo"
--- typescript-sort/src/index.ts
+++ typescript-sort/src/index.ts
@@ -1,3 +1,4 @@
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
@@ -5,3 +6,8 @@ const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data); // Output: [-5, 0, 3, 10]
+
+const charactersCollection = new CharactersCollection("Hello!");
+const sorterCharacters = new Sorter(charactersCollection);
+sorterCharacters.sort();
+console.log(charactersCollection.data); // Output: "!eHllo"
