18db7dcf704f94b465cbaf0666fe0355f68dc17c
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-22 15:59:34 +0530

    Configuring The TS Compiler & Concurrent Compilation and Execution

---
+++ typescript-sort/build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log(123);
+const logSomething = () => console.log("Hello World!");
+logSomething();
---
+++ typescript-sort/src/index.ts
@@ -0,0 +1,4 @@
+console.log(123);
+
+const logSomething = () => console.log("Hello World!");
+logSomething();
