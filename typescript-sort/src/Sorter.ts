aa9e8af6753b8e832fffe8a356fc24c6fae9bf87
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-22 19:39:21 +0530

    Integrating The Sort Method & Issues With Inheritance

--- typescript-sort/src/Sorter.ts
+++ typescript-sort/src/Sorter.ts
@@ -5,15 +5,13 @@ interface SORTABLE {
 }
 
 export class Sorter {
-  constructor(public collection: SORTABLE) {}
-
   sort(): void {
-    const { length } = this.collection;
+    const { length, compare, swap } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (compare(j, j + 1)) {
+          swap(j, j + 1);
         }
       }
     }
