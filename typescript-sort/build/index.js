b82f6c569d9492a76ce634f61c56880c9d420b80
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-22 17:26:42 +0530

    Typescript is Really Smart, Type Guards & Why it This Bad?

--- typescript-sort/build/index.js
+++ typescript-sort/build/index.js
@@ -7,10 +7,16 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                //* If Collection is an Array of Numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                //* If Collection is a String
+                if (typeof this.collection === "string") {
                 }
             }
         }
