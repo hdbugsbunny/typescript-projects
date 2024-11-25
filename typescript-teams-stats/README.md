6da01eff2cb8a3ff94cd018ce0a3581d6fe12869
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-25 12:39:19 +0530

    Losing Dataset Context, Using Enums & When to Use Enums

---
+++ typescript-teams-stats/README.md
@@ -0,0 +1,12 @@
+# TypeScript Teams Stats
+
+## What is an Enum?
+
+Enums (short for "enumerations") in TypeScript are a way to define a set of named constants. They allow you to create meaningful names for numeric or string values, making your code more readable and maintainable.
+
+## When to Use Enums?
+
+1. Follow near-identical syntax rules as normal objects
+2. Creates an object with the same keys and values when converted from TS to JS
+3. Primary goal is to signal to other engineers that these are all closely related values
+4. Use whenever we have a small fixed set of values that are all closely related and known at compile time
--- typescript-teams-stats/build/index.js
+++ typescript-teams-stats/build/index.js
@@ -8,12 +8,19 @@ const matches = fs_1.default
     .readFileSync("football.csv", { encoding: "utf-8" })
     .split("\n")
     .map((row) => row.split(","));
+//* ENUM - Enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 let manUnitedWins = 0;
 for (let match of matches) {
-    if (match[1] === "Man United" && match[5] === "H") {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === "A") {
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
