b899ca66bebd0b73a78b1f03a71170703ac97bd9
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-25 12:04:00 +0530

    Type Definition Files & Reading CSV Files

--- typescript-teams-stats/build/index.js
+++ typescript-teams-stats/build/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello World!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync("football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .slice(1)
+    .map((row) => row.split(","));
+console.log("🚀 ~ matches:", matches);
--- typescript-teams-stats/src/index.ts
+++ typescript-teams-stats/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+import fs from "fs";
+
+const matches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .slice(1)
+  .map((row: string): string[] => row.split(","));
+console.log("🚀 ~ matches:", matches);
