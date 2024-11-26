e67e437139f61e96cc281d81b3d0af717082a72a
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-26 18:50:52 +0530

    Extracting an Attribute Class, The Get Method's Shortcoming & An Advanced Generic Constraint

--- typescript-web/src/models/User.ts
+++ typescript-web/src/models/User.ts
@@ -12,14 +12,4 @@ const ROOT_URL = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public syncs: Sync<USERPROPS> = new Sync<USERPROPS>(ROOT_URL);
-
-  constructor(private data: USERPROPS) {}
-
-  get(propName: string): string | number | undefined {
-    return this.data[propName];
-  }
-
-  set(update: USERPROPS): void {
-    Object.assign(this.data, update);
-  }
 }
