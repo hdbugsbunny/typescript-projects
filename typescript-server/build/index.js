9d82bbd612107cc533c843233621d554352b8a1b
hdbugsbunny <harshitpal26697@gmail.com>
2024-12-13 12:19:21 +0530

    Using an Express Router

--- typescript-server/build/index.js
+++ typescript-server/build/index.js
@@ -4,9 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const loginRoutes_1 = require("./routes/loginRoutes");
 const app = (0, express_1.default)();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use(loginRoutes_1.router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- typescript-server/src/index.ts
+++ typescript-server/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { router } from "./routes/loginRoutes";
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.use(router);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
