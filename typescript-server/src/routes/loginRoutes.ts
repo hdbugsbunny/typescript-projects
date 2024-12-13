a7371137e78fe8bbb9aadf4885d9c826db646d08
hdbugsbunny <harshitpal26697@gmail.com>
2024-12-13 13:05:09 +0530

    Why Doesn't Express Play Nicely With TS?, Issues With Type Definition Files & Dealing With Poor Type Defs

--- typescript-server/src/routes/loginRoutes.ts
+++ typescript-server/src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from "express";
 
+interface RequestBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (_: Request, res: Response) => {
@@ -7,26 +11,32 @@ router.get("/login", (_: Request, res: Response) => {
     <form method="POST">
       <div>
         <label for="email">User Email:</label>
-        <input type="email" id="email" name="email" required>
+        <input type="email" id="email" name="email">
       </div>
       <div>
         <label for="password">User Password:</label>
-        <input type="password" id="password" name="password" required>
+        <input type="password" id="password" name="password">
       </div>
       <button>Login</button>
     </form>
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestBody, res: Response) => {
   //! Add your login logic here
   const { email, password } = req.body;
   //! Validate email and password
-  //   if (email === "example@email.com" && password === "password") {
-  res.send(`Login successful! Email: ${email}, Password: ${password}`);
-  //   } else {
-  // res.status(401).send("Invalid email or password.");
-  //   }
+  if (!email || !password) {
+    res.status(400).send("Email and Password are Required!");
+  } else if (email === "test2@test.com" && password === "1234567890") {
+    res
+      .status(200)
+      .send(
+        `Login successful! Email: ${email.toUpperCase()}, Password: ${password}`
+      );
+  } else {
+    res.status(401).send("Invalid Email or Password!");
+  }
 });
 
 export { router };
